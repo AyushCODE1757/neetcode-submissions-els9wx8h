36e4cd2afcfffe08fd6a083759a80d540eecc4a7
AyushCodes <aayush.kamble9b@gmail.com>
2026-08-08 21:28:52 +0530

    Add: car-pooling - submission-0

---
+++ Data Structures & Algorithms/car-pooling/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        passChange = [0] * 1001
+        for t in trips:
+            num, start, end = t
+            passChange[start] += num
+            passChange[end] -= num
+        curPass = 0
+        for i in range(1001):
+            curPass += passChange[i]
+            if curPass > capacity:
+                return False
+        return True
+        
